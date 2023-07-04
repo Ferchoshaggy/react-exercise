7cd766ea47ca5dc4ed3b9be82a52baf8067c9037
unknown <fernandoshaggy562@gmail.com>
2023-07-03 21:56:40 -0600

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,18 @@
+
+export const tasks = [
+  {
+    id: 1,
+    title: "Mi primer tarea",
+    description: "Esta es la primera tarea",
+  },
+  {
+    id: 2,
+    title: "Mi segunda tarea",
+    description: "Esta es la segunda tarea",
+  },
+  {
+    id: 3,
+    title: "Mi tercer tarea",
+    description: "Esta es la tercera tarea",
+  },
+];
